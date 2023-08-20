f26208cbee1fc28e6ab2bdf14d7fa0f942b6274d
almeidastor <regianealmeidastor@gmail.com>
2023-08-20 12:31:53 -0300

    att atualizacao 01

--- conteudo01.py
+++ conteudo01.py
@@ -100,3 +100,41 @@ print ('{2} {0} {0} tem {1} anos e seu imc é {2:2f}'.format(nome, idade, imc)) 
 print ('{n} tem {i} anos e seu imc é {im:2f}'.format(n=nome, i=idade, im=imc)) #formatação por letra
 
 
+#1.7.DESAFIO PRÁTICO (TESTE DE CONHECIMENTO ATÉ AQUI)
+"""
+* Criar variaveis para nome (str) e idade (int)
+* Altura (float) e peso (float) de uma pessoa
+* Criar uma variavel com o ano atual
+* Obter o ano de nascimento de uma pessoa (baseado na idade e no ano atual)
+* Obter o imc de uma pessoa com 2 casas decimais (peso e altura da pessoa)
+* Exibir um texto com todos os valores na tela usando F-String (com as chaves)
+"""
+nome = 'Regiane'
+idade = 29
+altura = 1.75
+peso = 75
+ano_atual = 2023
+ano_nascimento = ano_atual - idade
+imc = peso / (altura ** 2)
+
+print('meu nome é {}, {} de idade, altura: {}, ano de nascimento: {}, ano atual: {}, ano de nascimento {}, imc: {:2f}'.format(nome, idade, altura, peso, ano_atual, ano_nascimento, imc))
+
+
+#1.8.INPUT ENTRADA DE DADOS DO USUÁRIO
+
+input("Qual seu nome? ") #Exibe o cursor para digitar
+
+nome = input('Qual seu nome? ')
+idade  = input('Qual sua idade? ')
+ano_nascimento = 2023 - int(idade) #converte idade para um inteiro
+
+print(f'{nome} tem {idade} anos e nasceu em {ano_nascimento}')
+
+#1.8.1.CALCULADORA QUE SÓ FAZ SOMA
+
+numero_1 = int(input('Digite um número: ')) #conversão direta pra numero
+numero_2 = int(input('Digite outro número: ')) #conversão direta pra numero
+
+print (numero_1 + numero_2)
+
+
