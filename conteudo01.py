f19c9b9dd58e2971771e50cd7c39f0bff41905f6
almeidastor <regianealmeidastor@gmail.com>
2023-08-20 10:47:49 -0300

    aula01python

--- conteudo01.py
+++ conteudo01.py
@@ -49,6 +49,54 @@ print(r'Esse é meu \n texto (str).') #Não executa códigos contidos como coman
 
 
 
+#1.4.OPERADORES ARITMÉTICOS
+"""
++ : Soma ou concatenação
+- : Subtração
+* : Multiplicação
+/ : Divisão
+// : Divisão inteira
+** : Exponenciação
+% : Resto da divisão
+() : Parenteses - Precedencia
+"""
 
+print (10 * 10) #Multiplicação
+print (10 - 5) #Subtração
+print ((5 + 2) * 10) #Precedencia
+
+
+
+#1.5.VARIÁVEIS
+"""
+Iniciar com letras
+Pode conter números
+Separar com _
+Letras Minúsculas
+"""
+nome = 'Luiz'
+idade = 32
+altura = 1.80
+e_maior = idade > 18
+data_1 = True
+data_atual = 2023
+peso = 80
+imc = peso / (altura**2)
+
+print ('Nome:', nome)
+print ('Idade:', idade)
+print ('Altura:', altura)
+print ('É Maior', e_maior)
+
+#1.5.1.CÁLCULO DE IMC
+print (nome, 'tem', idade, 'anos de idade, e seu imc é', imc)
+
+
+#1.6.INTRODUÇÃO A FORMATAÇÃO DE STRINGS
+print (f'{nome} tem {idade} anos de idade, e seu imc é {imc}') #exibe a mesma formula
+print (f'{nome} tem {idade} anos de idade, e seu imc é {imc:.2f}') #altera a quantidade de casas decimais para 2
+print ('{} tem {} anos de idade e seu imc é {:2f}'.format(nome, idade, imc)) #formatação por nome
+print ('{2} {0} {0} tem {1} anos e seu imc é {2:2f}'.format(nome, idade, imc)) #formatação por numero
+print ('{n} tem {i} anos e seu imc é {im:2f}'.format(n=nome, i=idade, im=imc)) #formatação por letra
 
 
